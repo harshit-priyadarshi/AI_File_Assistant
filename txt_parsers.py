9574bec7761f2ec7864472c98ba84787733e3c9b
harshit-priyadarshi <harshitpriyadarshi2106@gmail.com>
2026-07-04 23:46:29 +0530

    Created

--- txt_parsers.py
+++ txt_parsers.py
@@ -1,4 +1,14 @@
 def extract_text(file_path: str) -> str:
-    with open(file_path, "r") as file:
+    """
+    Reads a text file and returns its contents as a string.
+
+    Args:
+        file_path: Path to the .txt file.
+
+    Returns:
+        The contents of the file as a string.
+    """
+    
+    with open(file_path, "r", encoding="utf-8") as file:
         text = file.read()
     return text
